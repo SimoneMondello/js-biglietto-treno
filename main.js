ff03b73c3e25769dfa85ea11962484b7f313db1b
simone <mondellosimo@gmail.com>
2022-03-22 16:13:12 +0100

    percentuali.sconto

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
 const età = prompt('Inserisci la tua età');
-
-
 let numerokm= prompt('Inserisci numero km da percorrere');
+const price = 0.21;    // Prezzo
+const fullPrice = numerokm*price; // numero scelto da cliente
+ 
+let totalPrice 
 
-
-
-
-
-    const price = 0.21;    // Price of km
-    const taxRate = numerokm; // numero scelto da cliente
     if(età <18) {
-    alert('Sconto 20%')        
+    alert('Sconto 20%')  
+    totalPrice = fullPrice -(fullPrice*20/100);
 } 
-
-
 else if (età >60) {
-    alert('Sconto 40%') }
-    // Calculate total to two decimal places
-    let totalPrice = price + (price * taxRate);
-    totalPrice.toFixed(2);
+    alert('Sconto 40%') 
+    totalPrice = fullPrice -(fullPrice*40/100); 
+}
 
+else {
+    alert('Prezzo Pieno')
+    totalPrice= fullPrice;
+}
+   
+   
     
     
     
     console.log("Total:", totalPrice);
+
+   
 
